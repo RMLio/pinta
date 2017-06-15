8b4994b4a9fc4879b645601f377c50a12a21c773
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2017-06-15 12:41:43 +0200

    update author

--- test/browser/create-browser-tests.sh
+++ test/browser/create-browser-tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Created by Pieter Heyvaert.
+# author: Pieter Heyvaert (pheyvaer.heyvaert@ugent.be)
 # Ghent University - imec - IDLab
 
 ./node_modules/browserify/bin/cmd.js ./test/node/search.js -o ./test/browser/search.js

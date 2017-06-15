a5a1c65bc1ce08c61669c9f0e5468f237f012cc9
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2017-06-15 13:24:47 +0200

    search all values and paths in json object

--- test/node/searchvaluesandpaths.js
+++ test/node/searchvaluesandpaths.js
@@ -5,7 +5,6 @@
 
 let assert = require('chai').assert;
 let search = require('../../index.js').json.searchValuesAndPaths;
-let results = require('./results-search.json').results;
 
 describe('searchValuesAndPaths', function () {
   it.only('#1', function () {
@@ -21,61 +20,10 @@ describe('searchValuesAndPaths', function () {
       ]
     };
 
-    let result = search(dataObject, values);
+    let expectedResult = [{"path":"$.persons[0].name.first","value":"pieter"},{"path":"$.persons[0].name.last","value":"heyvaert"},{"path":"$.persons[0].age","value":"26"}];
+    let result = search(dataObject);
     //console.log(JSON.stringify(result));
 
-    assert.equal(result.iterator, results[0].iterator, 'Iterator is incorrect.');
-    assert.deepEqual(result.paths, results[0].paths, 'Paths are incorrect.');
-  });
-
-  it('#2', function () {
-    let values = ['pieter', 'heyvaert', '26'];
-    let dataObject = {
-      persons: [
-        {
-          names: [{
-            first: 'pieter',
-            last: 'heyvaert'
-          }],
-          age: '26'
-        }
-      ]
-    };
-
-    let result = search(dataObject, values);
-    //console.log(result);
-
-    assert.equal(result.iterator, results[1].iterator, 'Iterator is incorrect.');
-    assert.deepEqual(result.paths, results[1].paths, 'Paths are incorrect.');
-  });
-
-  it('#3', function () {
-    let values = ['AAA'];
-    let dataObject = {
-      content: [
-        {Author: 'K. I.', title: 'AAA'},
-        {Author: 'Z. D.', title: 'BBB'}
-      ]
-    };
-
-    let result = search(dataObject, values);
-    //console.log(result);
-
-    assert.equal(result.iterator, results[2].iterator, 'Iterator is incorrect.');
-    assert.deepEqual(result.paths, results[2].paths, 'Paths are incorrect.');
-  });
-
-  it('#4', function () {
-    let values = [ '0'];
-    let dataObject = { persons:
-      [ { ID: 0, name: 'John', age: '30', friend_id: 1 },
-        { ID: 1, name: 'James', age: '25', friend_id: 2 } ] }
-    ;
-
-    let result = search(dataObject, values);
-    //console.log(result);
-
-    assert.equal(result.iterator, results[3].iterator, 'Iterator is incorrect.');
-    assert.deepEqual(result.paths, results[3].paths, 'Paths are incorrect.');
+    assert.deepEqual(result, expectedResult);
   });
 });
